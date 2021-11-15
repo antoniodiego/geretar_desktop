b2bb4b219706c6724000512e9284a948bfabb2bd
Antônio Diego Silva <antonio@gestaointeligentesoftware.com.br>
2021-11-14 23:38:41 -0300

    Configurei duas colunas (comentário e status)

--- src/main/java/br/com/antoniodiego/gertarefas/model/ModeloTabelaTarefasLista.java
+++ src/main/java/br/com/antoniodiego/gertarefas/model/ModeloTabelaTarefasLista.java
@@ -1,6 +1,7 @@
 package br.com.antoniodiego.gertarefas.model;
 
 import static br.com.antoniodiego.gertarefas.controller.JanelaPrincipalMatisseController.LOG_CONTR_PRINC;
+import br.com.antoniodiego.gertarefas.persist.daos.DAOTarefa;
 import br.com.antoniodiego.gertarefas.pojo.Tarefa;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -22,17 +23,17 @@ public class ModeloTabelaTarefasLista extends AbstractTableModel {
         "Título",
         "Data de criação", "Prioridade", "Data fazer", "Data modif",
         "Concluída",
-        "Posição"};
+        "Posição", "Comentário", "Status"};
 
     public static final Class[] CLASSES_COLUNAS = new Class[]{Long.class,
         Long.class,
         String.class,
-        LocalDate.class, Integer.class, LocalDate.class, LocalDateTime.class, Boolean.class, Integer.class};
+        LocalDate.class, Integer.class, LocalDate.class, LocalDateTime.class, Boolean.class, Integer.class, String.class, String.class};
     private List<Tarefa> tarefas;
     //  private final JanelaPrincipalController contr;
     public static final Boolean[] EDITAVEL = new Boolean[]{false, true, true,
         false,
-        true, true, false, true, true};
+        true, true, false, true, true, true, true};
 
     /**
      *
@@ -92,6 +93,11 @@ public class ModeloTabelaTarefasLista extends AbstractTableModel {
                 return tarefaLinha.isConcluida();
             case 8:
                 return tarefaLinha.getPosicao();
+            case 9:
+                return tarefaLinha.getComentario();
+            case 10:
+                return tarefaLinha.getStatus();
+
             default:
                 //TODO: poderia ser lan uma excessão
                 return null;
@@ -101,6 +107,7 @@ public class ModeloTabelaTarefasLista extends AbstractTableModel {
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
         Tarefa tarefaLinha = tarefas.get(rowIndex);
+
         switch (columnIndex) {
             case 0:
                 //tarefaLinha.setId((Long) aValue);
@@ -132,7 +139,15 @@ public class ModeloTabelaTarefasLista extends AbstractTableModel {
             case 8:
                 tarefaLinha.setPosicao((Integer) aValue);
                 break;
+            case 9:
+                tarefaLinha.setComentario((String) aValue);
+                break;
+            case 10:
+                tarefaLinha.setStatus((String) aValue);
+                break;
         }
+        DAOTarefa daoT = new DAOTarefa();
+        daoT.atualiza(tarefaLinha);
 
 //        /*Nessa chamada de método a alteração do id pode falhar.
 //        
