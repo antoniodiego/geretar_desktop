8cb623ff0f0f297ed0484a2a13a0cc671c82bc41
Antônio Diego Silva <antonio@gestaointeligentesoftware.com.br>
2021-11-16 16:23:49 -0300

    Removi imports não usados

--- src/main/java/br/com/antoniodiego/gertarefas/controller/Principal.java
+++ src/main/java/br/com/antoniodiego/gertarefas/controller/Principal.java
@@ -1,33 +1,8 @@
 package br.com.antoniodiego.gertarefas.controller;
 
-import static br.com.antoniodiego.gertarefas.controller.JanelaPrincipalController.LOG_CONTR_PRINC;
-import br.com.antoniodiego.gertarefas.persist.daos.DAOTarefa;
-import br.com.antoniodiego.gertarefas.pojo.GrupoTarefas;
-import br.com.antoniodiego.gertarefas.pojo.Tarefa;
-import br.com.antoniodiego.gertarefas.util.HibernateUtil;
 import br.com.antoniodiego.gertarefas.view.principal.JanelaPrincipal;
-import br.com.antoniodiego.gertarefas.view.principal.JanelaPrincipalMatisse;
-import java.awt.EventQueue;
-import java.net.ConnectException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.logging.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.flywaydb.core.Flyway;
-import org.flywaydb.core.api.FlywayException;
-import org.hsqldb.HsqlException;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Ponto de entrada do programa
