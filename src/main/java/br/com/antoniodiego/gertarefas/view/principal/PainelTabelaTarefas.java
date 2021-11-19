7494e7e5f2ca40af021edd059339c8ad56766507
Antônio Diego Silva <antonio@gestaointeligentesoftware.com.br>
2021-11-19 08:57:41 -0300

    Configurei recup estado da tab. rela à larg das col na inic. do prog.

--- src/main/java/br/com/antoniodiego/gertarefas/view/principal/PainelTabelaTarefas.java
+++ src/main/java/br/com/antoniodiego/gertarefas/view/principal/PainelTabelaTarefas.java
@@ -5,9 +5,15 @@ import br.com.antoniodiego.gertarefas.model.ModeloTabelaTarefasLista;
 import br.com.antoniodiego.gertarefas.persist.daos.DAOTarefa;
 import br.com.antoniodiego.gertarefas.pojo.Tarefa;
 import br.com.antoniodiego.gertarefas.view.DialogoEditarTarefa;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.swing.ButtonGroup;
@@ -19,6 +25,9 @@ import javax.swing.SortOrder;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableRowSorter;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 
 /**
  * O painel expõe o modelo da tabela de tarefas para que os da
@@ -80,6 +89,7 @@ public class PainelTabelaTarefas extends javax.swing.JPanel {
                 col.setPreferredWidth(80);
             } else if (i == 4) {
                 col.setPreferredWidth(40);
+
             } else if (i == 5) {
                 col.setPreferredWidth(80);
             } else if (i == 6) {
@@ -88,11 +98,49 @@ public class PainelTabelaTarefas extends javax.swing.JPanel {
                 col.setPreferredWidth(40);
             } else if (i == 8) {
                 //Posição
-                col.setPreferredWidth(50);
+                col.setPreferredWidth(40);
             } else if (i == 9) {
                 col.setPreferredWidth(100);
             } else if (i == 10) {
                 col.setPreferredWidth(70);
+            }
+        }
+
+        File arquivoTam = new File("colunas.json");
+        if (arquivoTam.exists()) {
+            try {
+                FileReader fr = new FileReader(arquivoTam);
+                JSONParser js = new JSONParser(JSONParser.ACCEPT_NAN);
+                Object res = js.parse(fr);
+
+                if (res instanceof JSONObject) {
+                    JSONObject jsO = (JSONObject) res;
+
+                    jsO.entrySet().forEach((e) -> {
+                        LOG_CONTR_PRINC.debug("Key: {}", e.getKey());
+
+                        TableColumn coluna = tabelaTarefas.getColumn(e.getKey());
+                        LOG_CONTR_PRINC.debug("Alterando tam coluna " + e.getKey());
+                        JSONObject config = (JSONObject) jsO.get(e.getKey());
+
+                        coluna.setPreferredWidth(config.getAsNumber("width").intValue());
+                        //coluna.setModelIndex(config.getAsNumber("index").intValue());
+                    });
+                }
+
+//            StringBuilder leit = new StringBuilder();
+//            char[] cbuf = new char[1024];
+//            int len = 0;
+//            while ((len = fr.read(cbuf)) != -1) {
+//                leit.append(new String(cbuf, 0, len));
+//            }
+//            fr.close();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(PainelTabelaTarefas.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(PainelTabelaTarefas.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex) {
+                Logger.getLogger(PainelTabelaTarefas.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
@@ -188,7 +236,6 @@ public class PainelTabelaTarefas extends javax.swing.JPanel {
                 return canEdit [columnIndex];
             }
         });
-        tabelaTarefas.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_NEXT_COLUMN);
         scrollPaneTabela.setViewportView(tabelaTarefas);
 
         painelTabela.add(scrollPaneTabela);
