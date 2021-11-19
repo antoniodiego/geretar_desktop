2aa4897aa07c9fa9ec221ce1cd115227b979e461
Antônio Diego Silva <antonio@gestaointeligentesoftware.com.br>
2021-11-18 23:50:27 -0300

    Mudei para o estado de desenv

--- src/main/java/br/com/antoniodiego/gertarefas/controller/Principal.java
+++ src/main/java/br/com/antoniodiego/gertarefas/controller/Principal.java
@@ -11,7 +11,10 @@ import org.apache.logging.log4j.Logger;
  */
 public class Principal {
 
-    public static boolean DESENV = false;
+    /**
+     *
+     */
+    public static boolean DESENV = true;
 
     private static final Logger LOG_PRINC = LogManager.
             getLogger(JanelaPrincipal.class);
