3832c7cd31cc86d7b4773eb4df4a799cd38fd23a
Antonio Diêgo Silva <antoniodiegoluz@hotmail.com>
2022-02-23 23:29:54 -0300

    Configurei loggin para arquivos Corrigi problema com o campo id na hora de editar tarefas

--- src/main/java/br/com/antoniodiego/gertarefas/telas/principal/JanelaPrincipalMatisse.java
+++ src/main/java/br/com/antoniodiego/gertarefas/telas/principal/JanelaPrincipalMatisse.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.antoniodiego.gertarefas.telas.principal;
 
 import br.com.antoniodiego.gertarefas.controller.JanelaPrincipalMatisseController;
@@ -18,7 +14,7 @@ import net.minidev.json.JSONObject;
 
 /**
  *
- * @author antonio
+ * @author Antonio Diêgo
  */
 public class JanelaPrincipalMatisse extends javax.swing.JFrame {
 
